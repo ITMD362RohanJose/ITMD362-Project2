cf70d5a3779606d9d6cf9457638831ae9e9d2676
Estefania <elopez17@hawk.iit.edu>
2021-05-04 13:43:30 -0500

    fixed indentation

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,7 +1,7 @@
 const mes = document.getElementById('mes')
 const form = document.getElementById('form')
 form.addEventListener('submit', (e) => {
-  if(mes.textLength <= 0){
+  if (mes.textLength <= 0) {
     alert("Message field cannot be left empty");
     e.preventDefault()
   }
